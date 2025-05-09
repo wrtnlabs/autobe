97f9ad27ba371fa0c303895720b8a83180c7a273
Jeongho Nam <samchon.github@gmail.com>
2025-05-09 14:50:44 +0900

    Auto fork of testing repositories

--- test/src/utils/TestRepositoryUtil.ts
+++ test/src/utils/TestRepositoryUtil.ts
@@ -1,21 +1,30 @@
+import cp from "child_process";
 import fs from "fs";
 
 import { TestGlobal } from "../TestGlobal";
 
 export namespace TestRepositoryUtil {
-  export const prisma = (project: string): Promise<Record<string, string>> =>
-    collect({
+  export const prisma = async (
+    project: string,
+  ): Promise<Record<string, string>> => {
+    await fork(project);
+    return collect({
       root: `${TestGlobal.ROOT}/assets/repositories/${project}/prisma/schema`,
       prefix: "",
       extension: "prisma",
     });
+  };
 
-  export const src = (project: string): Promise<Record<string, string>> =>
-    collect({
+  export const src = async (
+    project: string,
+  ): Promise<Record<string, string>> => {
+    await fork(project);
+    return collect({
       root: `${TestGlobal.ROOT}/assets/repositories/${project}/src`,
       prefix: "src/",
       extension: "ts",
     });
+  };
 
   const collect = async (props: {
     root: string;
@@ -36,5 +45,25 @@ export namespace TestRepositoryUtil {
     };
     await iterate(props.root);
     return output;
+  };
+
+  const fork = async (name: string): Promise<void> => {
+    const location: string = `${TestGlobal.ROOT}/assets/repositories/${name}`;
+    if (fs.existsSync(location))
+      cp.execSync("git pull", {
+        cwd: location,
+        stdio: "ignore",
+      });
+    else {
+      try {
+        await fs.promises.mkdir(`${TestGlobal.ROOT}/assets/repositories`, {
+          recursive: true,
+        });
+      } catch {}
+      cp.execSync(`git clone https://github.com/samchon/${name}`, {
+        cwd: `${TestGlobal.ROOT}/assets/repositories`,
+        stdio: "ignore",
+      });
+    }
   };
 }
