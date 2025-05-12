f533d728bbc08609ed9a7241afcb9a40ccea83a1
Jeongho Nam <samchon.github@gmail.com>
2025-05-12 13:55:52 +0900

    Add compiler (#11)

--- packages/agent/src/context/AutoBeContext.ts
+++ packages/agent/src/context/AutoBeContext.ts
@@ -1,3 +1,4 @@
+import { IAutoBeCompiler } from "@autobe/interface";
 import { ILlmSchema } from "@samchon/openapi";
 
 import { IAutoBeVendor } from "../structures/IAutoBeVendor";
@@ -7,6 +8,7 @@ import { AutoBeTokenUsage } from "./AutoBeTokenUsage";
 export interface AutoBeContext<Model extends ILlmSchema.Model> {
   model: Model;
   vendor: IAutoBeVendor;
+  compiler: IAutoBeCompiler;
   usage: () => AutoBeTokenUsage;
   state: () => AutoBeState;
 }
--- packages/agent/src/structures/IAutoBeProps.ts
+++ packages/agent/src/structures/IAutoBeProps.ts
@@ -1,4 +1,4 @@
-import { AutoBeHistory } from "@autobe/interface";
+import { AutoBeHistory, IAutoBeCompiler } from "@autobe/interface";
 import { ILlmSchema } from "@samchon/openapi";
 
 import { IAutoBeConfig } from "./IAutoBeConfig";
@@ -7,6 +7,7 @@ import { IAutoBeVendor } from "./IAutoBeVendor";
 export interface IAutoBeProps<Model extends ILlmSchema.Model> {
   model: Model;
   vendor: IAutoBeVendor;
+  compiler: IAutoBeCompiler;
   histories?: AutoBeHistory[] | undefined;
   config?: IAutoBeConfig | undefined;
 }
