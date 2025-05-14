422649f75ef0a25575f4529de6a9908834681da9
kakasoo <kscodebase@gmail.com>
2025-05-14 18:01:34 +0900

    test: manual test of analyze agent

---
+++ test/src/manual/AnalyzeAgent.ts
@@ -0,0 +1,72 @@
+import { config } from "dotenv";
+import fs from "fs";
+import OpenAI from "openai";
+import * as readline from "readline";
+
+import { testAgent } from "../../../packages/agent/src/analyze/AnalyzeAgent";
+
+config();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
+
+// 문장을 입력받는 부분
+const askQuestion = (prompt: string): Promise<string> => {
+  return new Promise((resolve) => {
+    rl.question(prompt, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+async function createAgent() {
+  const foldername = new Date().getTime().toString();
+  await fs.promises.mkdir(`${__dirname}/${foldername}`);
+
+  return testAgent(
+    {
+      api: new OpenAI({
+        apiKey: process.env.OPENAI_KEY,
+      }),
+      model: "gpt-4.1",
+    },
+    {
+      folder: `${__dirname}/${foldername}`,
+    },
+  );
+}
+
+async function main() {
+  const agent = await createAgent();
+  let input = "";
+  while (true) {
+    input = await askQuestion(
+      "문장을 입력하세요 (줄바꿈은 Shift+Enter로 가능, 종료하려면 Ctrl+C): ",
+    );
+    const inputBuffer = input + "\n"; // 줄바꿈과 함께 입력 내용 누적
+
+    // 현재까지 입력한 내용 출력
+    console.log(`User: ${inputBuffer}`);
+
+    // LLM 답변
+    const responses = await agent.conversate(inputBuffer);
+
+    for (const response of responses) {
+      const text =
+        response.type === "text"
+          ? response.text
+          : response.type === "describe"
+            ? response.text
+            : null;
+
+      if (text) {
+        console.log(`Agent: ${text}`);
+      }
+    }
+  }
+}
+
+main().catch(() => process.exit(-1));
