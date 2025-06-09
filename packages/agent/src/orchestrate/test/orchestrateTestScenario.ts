4b8c161a0053c57ac67d785b661144566af7c14b
kakasoo <kscodebase@gmail.com>
2025-06-09 17:13:15 +0900

    feat: add new inner agent for writing test scenario

--- packages/agent/src/orchestrate/test/orchestrateTestScenario.ts
+++ packages/agent/src/orchestrate/test/orchestrateTestScenario.ts
@@ -2,95 +2,79 @@ import { IAgenticaController, MicroAgentica } from "@agentica/core";
 import { AutoBeOpenApi, AutoBeTest } from "@autobe/interface";
 import { AutoBeTestScenarioEvent } from "@autobe/interface/src/events/AutoBeTestScenarioEvent";
 import { ILlmApplication, ILlmSchema } from "@samchon/openapi";
-import { HashMap, HashSet, IPointer } from "tstl";
+import { IPointer } from "tstl";
 import typia from "typia";
 
+import { AutoBeSystemPromptConstant } from "../../constants/AutoBeSystemPromptConstant";
 import { AutoBeContext } from "../../context/AutoBeContext";
 import { assertSchemaModel } from "../../context/assertSchemaModel";
-import { divideArray } from "../../utils/divideArray";
-import { OpenApiEndpointComparator } from "../interface/OpenApiEndpointComparator";
 import { transformTestScenarioHistories } from "./transformTestScenarioHistories";
 
 export async function orchestrateTestScenario<Model extends ILlmSchema.Model>(
   ctx: AutoBeContext<Model>,
-  endpoints: AutoBeOpenApi.IEndpoint[],
-  capacity: number = 12,
 ): Promise<AutoBeTestScenarioEvent> {
-  const matrix: AutoBeOpenApi.IEndpoint[][] = divideArray({
-    array: endpoints,
-    capacity,
-  });
+  const testFiles = Object.entries(ctx.state().interface?.files ?? {})
+    .filter(([filename]) => {
+      return filename.startsWith("test/features/api/");
+    })
+    .reduce<Record<string, string>>((acc, [filename, content]) => {
+      return Object.assign(acc, { [filename]: content });
+    }, {});
+
+  const operations = ctx.state().interface?.document.operations ?? [];
+  const endpoints: Omit<AutoBeOpenApi.IOperation, "specification">[] =
+    operations.map((it) => {
+      return {
+        method: it.method,
+        path: it.path,
+        summary: it.summary,
+        description: it.description,
+        parameters: it.parameters,
+        requestBody: it.requestBody,
+        responseBody: it.responseBody,
+      };
+    });
+
   const start: Date = new Date();
 
   let completed: number = 0;
 
   const scenarios: AutoBeTest.IScenario[][] = await Promise.all(
-    matrix.map(async (it) => {
-      const row: AutoBeTest.IScenario[] = await divideAndConquer(
+    endpoints.map(async (endpoint, i, arr) => {
+      const otherOperations = arr.filter((_el, j) => i !== j);
+      const rows: AutoBeTest.IScenario[] = await process(
         ctx,
-        it,
-        3,
-        (count) => {
-          completed += count;
-        },
+        endpoint,
+        otherOperations,
+        testFiles,
       );
       ctx.dispatch({
         type: "testScenario",
-        scenarios: row,
-        total: endpoints.length,
-        step: ctx.state().analyze?.step ?? 0,
+        scenarios: rows,
+        total: rows.flatMap((el) => el.scenarios).length,
+        step: ctx.state().test?.step ?? 0,
         completed,
         created_at: start.toISOString(),
       });
-      return row;
+      return rows;
     }),
   );
 
   return {
     type: "testScenario",
     scenarios: scenarios.flat(),
-    total: endpoints.length,
-    step: ctx.state().analyze?.step ?? 0,
+    total: scenarios.flat().flatMap((el) => el.scenarios).length,
+    step: ctx.state().test?.step ?? 0,
     completed,
     created_at: start.toISOString(),
   };
 }
 
-async function divideAndConquer<Model extends ILlmSchema.Model>(
-  ctx: AutoBeContext<Model>,
-  endpoints: AutoBeOpenApi.IEndpoint[],
-  retry: number,
-  progress: (completed: number) => void,
-): Promise<AutoBeTest.IScenario[]> {
-  const remained: HashSet<AutoBeOpenApi.IEndpoint> = new HashSet(
-    endpoints,
-    OpenApiEndpointComparator.hashCode,
-    OpenApiEndpointComparator.equals,
-  );
-  const senarios: HashMap<AutoBeOpenApi.IEndpoint, AutoBeTest.IScenario> =
-    new HashMap(
-      OpenApiEndpointComparator.hashCode,
-      OpenApiEndpointComparator.equals,
-    );
-  for (let i: number = 0; i < retry; ++i) {
-    if (remained.empty() === true || senarios.size() >= endpoints.length) break;
-    const before: number = senarios.size();
-    const newbie: AutoBeTest.IScenario[] = await process(
-      ctx,
-      Array.from(remained),
-    );
-    for (const item of newbie) {
-      senarios.set(item.endpoint, item);
-      remained.erase(item.endpoint);
-    }
-    if (senarios.size() - before !== 0) progress(senarios.size() - before);
-  }
-  return senarios.toJSON().map((it) => it.second);
-}
-
 async function process<Model extends ILlmSchema.Model>(
   ctx: AutoBeContext<Model>,
-  endpoints: AutoBeOpenApi.IEndpoint[],
+  endpoint: AutoBeOpenApi.IEndpoint,
+  others: AutoBeOpenApi.IEndpoint[],
+  testFiles: Record<string, string>,
 ): Promise<AutoBeTest.IScenario[]> {
   const pointer: IPointer<AutoBeTest.IScenario[] | null> = {
     value: null,
@@ -100,9 +84,46 @@ async function process<Model extends ILlmSchema.Model>(
     model: ctx.model,
     vendor: ctx.vendor,
     config: {
-      ...(ctx.config ?? {}),
+      ...(ctx.config ?? { locale: "en-US" }),
+      systemPrompt: {
+        describe: () => {
+          return "Answer only 'completion' or 'failure'.";
+        },
+      },
     },
-    histories: transformTestScenarioHistories(ctx.state()),
+    tokenUsage: ctx.usage(),
+    histories: [
+      ...transformTestScenarioHistories(ctx.state()),
+      {
+        type: "systemMessage",
+        text: AutoBeSystemPromptConstant.TEST,
+      },
+      {
+        type: "systemMessage",
+        text: [
+          `This is a description of different APIs.`,
+          `Different APIs may have to be called to create one.`,
+          `Check which functions have been developed.`,
+          "```json",
+          JSON.stringify(others, null, 2),
+          "```",
+        ].join("\n"),
+      },
+      {
+        type: "systemMessage",
+        text: [
+          "Below is basically the generated test code,",
+          "which is a test to verify that the API is simply called and successful.",
+          "Since there is already an automatically generated API,",
+          "when a user requests to create a test scenario, two or more APIs must be combined,",
+          "but a test in which the currently given endpoint is the main must be created.",
+          '"Input Test Files" should be selected from the list of files here.',
+          "```json",
+          JSON.stringify(testFiles, null, 2),
+          "```",
+        ].join("\n"),
+      },
+    ],
     controllers: [
       createApplication({
         model: ctx.model,
@@ -119,10 +140,10 @@ async function process<Model extends ILlmSchema.Model>(
 
   await agentica.conversate(
     [
-      "Make User Scenarios for below endpoints:",
+      "Make User Scenarios for below endpoint:",
       "",
       "```json",
-      JSON.stringify(Array.from(endpoints), null, 2),
+      JSON.stringify(endpoint, null, 2),
       "```",
     ].join("\n"),
   );
