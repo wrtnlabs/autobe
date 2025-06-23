e759665a7b528c7fa057949062d4b98307fef37a
kakasoo <kscodebase@gmail.com>
2025-06-23 19:41:44 +0900

    test: `test_agent_test_correct_bbs`

---
+++ test/src/features/test/test_agent_test_correct_bbs.ts
@@ -0,0 +1,4 @@
+import { validate_agent_test_correct } from "./internal/validate_agent_test_correct";
+
+export const test_agent_test_correct_bbs = () =>
+  validate_agent_test_correct("samchon", "bbs-backend");
