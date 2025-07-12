6680eb8467e6ed12e1aee726ce2f56fccc0aad38
Jeongho Nam <samchon.github@gmail.com>
2025-07-13 01:50:08 +0900

    fix(compiler): exact template files.

--- test/src/executable/typeName.ts
+++
@@ -1,5 +0,0 @@
-import { AutoBeTest } from "@autobe/interface";
-import typia, { IJsonSchemaUnit } from "typia";
-
-const unit: IJsonSchemaUnit = typia.json.schema<AutoBeTest.IExpression>();
-console.log(unit.components.schemas?.["AutoBeTest.IExpression"]);
