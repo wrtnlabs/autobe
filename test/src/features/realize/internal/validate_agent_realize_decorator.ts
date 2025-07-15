e201fda1b834f4f1a98394faaac90b8f1a6c0c54
michael <7471919@naver.com>
2025-07-15 16:34:36 +0900

    test: define test_agent_realize_decorator_bbs

---
+++ test/src/features/realize/internal/validate_agent_realize_decorator.ts
@@ -0,0 +1,102 @@
+import { orchestrateRealizeDecorator } from "@autobe/agent/src/orchestrate/realize/orchestrateRealizeDecorator";
+import { FileSystemIterator } from "@autobe/filesystem";
+import { AutoBeEvent } from "@autobe/interface";
+import fs from "fs/promises";
+import path from "path";
+import typia from "typia";
+
+import { TestFactory } from "../../../TestFactory";
+import { TestGlobal } from "../../../TestGlobal";
+import { TestProject } from "../../../structures/TestProject";
+import { prepare_agent_realize } from "./prepare_agent_realize";
+
+export const validate_agent_realize_decorator = async (
+  factory: TestFactory,
+  project: TestProject,
+) => {
+  if (TestGlobal.env.CHATGPT_API_KEY === undefined) return false;
+
+  // PREPARE AGENT
+  const { agent } = await prepare_agent_realize(factory, project);
+
+  const map = new Map<string, true>();
+  const events: AutoBeEvent[] = [];
+  const enroll = (event: AutoBeEvent) => {
+    if (!map.has(event.type)) {
+      map.set(event.type, true);
+    }
+
+    events.push(event);
+  };
+
+  agent.on("realizeStart", enroll);
+  agent.on("realizeDecorator", enroll);
+  agent.on("realizeProgress", enroll);
+  agent.on("realizeValidate", enroll);
+  agent.on("realizeComplete", enroll);
+
+  const ctx = agent.getContext();
+
+  ctx.state().interface?.document.operations.forEach((op, i) => {
+    if (!op.authorization?.role) {
+      op.authorization = {
+        role: [],
+        type: "Bearer",
+      };
+    }
+
+    if (i === 1) op.authorization.role.push("user");
+    if (i === 2) op.authorization.role.push("admin");
+    if (i === 3) op.authorization.role.push("moderator");
+  });
+
+  const result = await orchestrateRealizeDecorator(ctx);
+
+  const prisma = ctx.state().prisma?.compiled;
+
+  const prismaClients: Record<string, string> =
+    prisma?.type === "success" ? prisma.nodeModules : {};
+
+  const files: Record<string, string> = {
+    "src/MyGlobal.ts": await fs.readFile(
+      path.join(__dirname, "../../../../../internals/template/src/MyGlobal.ts"),
+      "utf-8",
+    ),
+    "src/authentications/jwtAuthorize.ts": await fs.readFile(
+      path.join(
+        __dirname,
+        "../../../../../internals/template/src/providers/jwtAuthorize.ts",
+      ),
+      "utf-8",
+    ),
+    ...prismaClients,
+    ...result.reduce(
+      (acc, curr) => {
+        acc[`src/decorators/${curr.decorator.name}.ts`] = curr.decorator.code;
+        acc[`src/authentications/${curr.provider.name}.ts`] =
+          curr.provider.code;
+        return acc;
+      },
+      {} as Record<string, string>,
+    ),
+  };
+
+  const histories = agent.getHistories();
+
+  await FileSystemIterator.save({
+    root: `${TestGlobal.ROOT}/results/${project}/realize/decorator`,
+    files: {
+      ...(await agent.getFiles()),
+      ...files,
+      "logs/events.json": typia.json.stringify(events),
+      "logs/result.json": typia.json.stringify(result),
+      "logs/histories.json": typia.json.stringify(histories),
+    },
+  });
+
+  const compiled = await ctx.compiler.typescript.compile({ files });
+
+  if (compiled.type !== "success") {
+    throw new Error(JSON.stringify(compiled, null, 2));
+  }
+};
