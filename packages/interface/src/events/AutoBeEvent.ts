f877c0554bc77135eaaf49a396e91c1657252ac4
michael <7471919@naver.com>
2025-07-31 15:04:50 +0900

    chore: remove unnecessary import

--- packages/interface/src/events/AutoBeEvent.ts
+++ packages/interface/src/events/AutoBeEvent.ts
@@ -3,7 +3,6 @@ import { AutoBeAnalyzeReviewEvent } from "./AutoBeAnalyzeReviewEvent";
 import { AutoBeAnalyzeStartEvent } from "./AutoBeAnalyzeStartEvent";
 import { AutoBeAnalyzeWriteEvent } from "./AutoBeAnalyzeWriteEvent";
 import { AutoBeAssistantMessageEvent } from "./AutoBeAssistantMessageEvent";
-import { AutoBeExceptionEvent } from "./AutoBeExceptionEvent";
 import { AutoBeInterfaceComplementEvent } from "./AutoBeInterfaceComplementEvent";
 import { AutoBeInterfaceCompleteEvent } from "./AutoBeInterfaceCompleteEvent";
 import { AutoBeInterfaceEndpointsEvent } from "./AutoBeInterfaceEndpointsEvent";
