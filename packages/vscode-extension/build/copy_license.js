b7d4b7dd438f4839550123da934e0de51d0ba7de
sunrabbit123 <qudwls185@naver.com>
2025-08-04 14:09:12 +0900

    update license script
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/vscode-extension/build/copy_license.js
+++ packages/vscode-extension/build/copy_license.js
@@ -4,4 +4,7 @@ const path = require("path");
 const licensePath = path.join(__dirname, "../../../LICENSE");
 const targetPath = path.join(__dirname, "../LICENSE");
 
+if(fs.existsSync(targetPath)) {
+  return;
+}
 fs.copyFileSync(licensePath, targetPath);
