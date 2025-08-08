b3f2a93c914ac92c9416498362805b1857229c8b
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-08-09 00:16:11 +0900

    fix build errors

--- packages/vscode-extension/rspack.config.js
+++ packages/vscode-extension/rspack.config.js
@@ -40,6 +40,12 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false
+        }
+      },
     ],
   },
   node: {
