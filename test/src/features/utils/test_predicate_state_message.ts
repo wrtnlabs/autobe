553f01890ade6b5b9991f1ce9535409beee7f9c3
Jeongho Nam <samchon.github@gmail.com>
2025-08-15 01:40:26 +0900

    feat(agent): force function calling in the prompt

--- test/src/features/utils/test_predicate_state_message.ts
+++ test/src/features/utils/test_predicate_state_message.ts
@@ -6,6 +6,20 @@ import typia from "typia";
 type Step = "analyze" | "prisma" | "interface" | "test" | "realize";
 
 export const test_predicate_state_message = (): void => {
+  typia.misc.literals<Step>().forEach((y) => {
+    const state: AutoBeState = {
+      analyze: null,
+      prisma: null,
+      interface: null,
+      test: null,
+      realize: null,
+    };
+    const message: string | null = predicateStateMessage(state, y);
+    const expected: boolean = y === "analyze";
+    const actual: boolean = message === null;
+    TestValidator.equals(`null -> ${y}`)(expected)(actual);
+  });
+
   typia.misc.literals<Step>().forEach((x, i, array) => {
     typia.misc.literals<Step>().forEach((y, j) => {
       const state: AutoBeState = {
@@ -17,9 +31,9 @@ export const test_predicate_state_message = (): void => {
       };
       for (const key of array.slice(0, i + 1)) state[key] = { step: 0 } as any;
       const message: string | null = predicateStateMessage(state, y);
-      const possible: boolean = message === null;
-      if (i >= j + 1)
-        TestValidator.equals(`${x} -> ${y}`)(possible)(i >= j + 1);
+      const expected: boolean = i >= j - 1;
+      const actual: boolean = message === null;
+      TestValidator.equals(`${x} -> ${y}`)(expected)(actual);
     });
   });
 };
