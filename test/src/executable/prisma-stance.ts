30e977f8301d18b00be59aa3df8dc70096a2c9c1
Jeongho Nam <samchon.github@gmail.com>
2025-08-21 13:40:18 +0900

    Fix prisma example files

--- test/src/executable/prisma-stance.ts
+++ test/src/executable/prisma-stance.ts
@@ -7,13 +7,22 @@ const DIRECTORY = `${TestGlobal.ROOT}/src/features/compiler/examples`;
 
 const main = async (): Promise<void> => {
   for (const file of await fs.promises.readdir(DIRECTORY)) {
-    if (file.endsWith(".prisma.json") === false) continue;
+    console.log(file);
+    if (
+      file.startsWith("prisma.") === false ||
+      file.endsWith(".json") === false
+    )
+      continue;
     const location: string = `${DIRECTORY}/${file}`;
     const application: AutoBePrisma.IApplication = JSON.parse(
       await fs.promises.readFile(location, "utf-8"),
     );
     for (const model of application.files.map((f) => f.models).flat())
-      model.stance = "primary";
+      model.stance = model.name.endsWith("_snapshots")
+        ? "snapshot"
+        : model.name.includes("_snapshot_")
+          ? "subsidiary"
+          : "primary";
     await fs.promises.writeFile(location, JSON.stringify(application), "utf8");
   }
 };
