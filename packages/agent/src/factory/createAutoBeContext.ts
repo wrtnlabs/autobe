b9914b2458cad6cede1099582a97cf6655888434
Jeongho Nam <samchon.github@gmail.com>
2025-08-26 01:16:12 +0900

    archiving continuously

--- packages/agent/src/factory/createAutoBeContext.ts
+++ packages/agent/src/factory/createAutoBeContext.ts
@@ -134,7 +134,6 @@ export const createAutoBeContext = <Model extends ILlmSchema.Model>(props: {
           histories.some((h) => h.type === "execute" && h.success === true)
       ) {
         const failure = () => {
-          console.log(JSON.stringify(histories, null, 2));
           throw new Error(
             `Failed to function calling in the ${next.source} step`,
           );
