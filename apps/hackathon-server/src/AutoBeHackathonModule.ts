8b921019ecc592f97fbf6dfd1dcf4067602c4394
Jeongho Nam <samchon.github@gmail.com>
2025-09-11 15:01:29 +0900

    feat(hackathon): support pm2

--- apps/hackathon-server/src/AutoBeHackathonModule.ts
+++ apps/hackathon-server/src/AutoBeHackathonModule.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { AppController } from "./controllers/AppController";
 import { AutoBeHackathonModeratorModule } from "./controllers/moderators/AutoBeHackathonModeratorModule";
 import { AutoBeHackathonParticipantModule } from "./controllers/participants/AutoBeHackathonParticipantModule";
 
 @Module({
   imports: [AutoBeHackathonModeratorModule, AutoBeHackathonParticipantModule],
+  controllers: [AppController],
 })
 export class AutoBeHackathonModule {}
