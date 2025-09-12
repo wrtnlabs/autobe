a3833acfd2625aae808186dd35634839a9ed8c96
Jeongho Nam <samchon.github@gmail.com>
2025-09-12 21:03:36 +0900

    fix(hackathon): rollback pm2

--- apps/hackathon-server/ecosystem.config.js
+++ apps/hackathon-server/ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: "hackathon-server",
-      script: "pnpm",
-      args: "run start",
+      script: "src/executable/server.ts",
+      interpreter: "pnpm",
+      interpreter_args: "-r ts-node/register",
       instances: 4,
       exec_mode: "cluster",
     },
