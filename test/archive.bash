a631f9c685416a914339394fc25328692fb70ef9
Jeongho Nam <samchon.github@gmail.com>
2025-09-12 23:13:48 +0900

    feat(test): new histories of `qwen3-next-80b-a3b`

--- test/archive.bash
+++ test/archive.bash
@@ -29,25 +29,25 @@ pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project shopping > 
 # SPECIAL PHASES
 ################################################
 # openai/gpt-4.1
-pnpm run archive --vendor openai/gpt-4.1 --project todo --from interface > archive.gpt-big-todo.log
-pnpm run archive --vendor openai/gpt-4.1 --project reddit --from interface > archive.gpt-big-reddit.log
-pnpm run archive --vendor openai/gpt-4.1 --project bbs --from interface > archive.gpt-big-bbs.log
-pnpm run archive --vendor openai/gpt-4.1 --project shopping --from interface > archive.gpt-big-shopping.log
+pnpm run archive --vendor openai/gpt-4.1 --project todo --from realize > archive.gpt-big-todo.log
+pnpm run archive --vendor openai/gpt-4.1 --project reddit --from realize > archive.gpt-big-reddit.log
+pnpm run archive --vendor openai/gpt-4.1 --project bbs --from realize > archive.gpt-big-bbs.log
+pnpm run archive --vendor openai/gpt-4.1 --project shopping --from realize > archive.gpt-big-shopping.log
 
 # openai/gpt-4.1-mini
-pnpm run archive --vendor openai/gpt-4.1-mini --project todo --from interface > archive.gpt-mini-todo.log
-pnpm run archive --vendor openai/gpt-4.1-mini --project reddit --from interface > archive.gpt-mini-reddit.log
-pnpm run archive --vendor openai/gpt-4.1-mini --project bbs --from interface > archive.gpt-mini-bbs.log
-pnpm run archive --vendor openai/gpt-4.1-mini --project shopping --from interface > archive.gpt-mini-shopping.log
+pnpm run archive --vendor openai/gpt-4.1-mini --project todo --from realize > archive.gpt-mini-todo.log
+pnpm run archive --vendor openai/gpt-4.1-mini --project reddit --from realize > archive.gpt-mini-reddit.log
+pnpm run archive --vendor openai/gpt-4.1-mini --project bbs --from realize > archive.gpt-mini-bbs.log
+pnpm run archive --vendor openai/gpt-4.1-mini --project shopping --from realize > archive.gpt-mini-shopping.log
 
 # qwen/qwen3-235b-a22b-2507
 pnpm run archive --vendor qwen/qwen3-235b-a22b-2507 --project todo --from interface > archive.qwen3-235b-todo.log
-pnpm run archive --vendor qwen/qwen3-235b-a22b-2507 --project reddit --from interface > archive.qwen3-235b-reddit.log
+pnpm run archive --vendor qwen/qwen3-235b-a22b-2507 --project reddit --from test > archive.qwen3-235b-reddit.log
 pnpm run archive --vendor qwen/qwen3-235b-a22b-2507 --project bbs --from interface > archive.qwen3-235b-bbs.log
 pnpm run archive --vendor qwen/qwen3-235b-a22b-2507 --project shopping --from interface > archive.qwen-235b-shopping.log
 
 # qwen/qwen3-next-80b-a3b
-pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project todo --from interface > archive.qwen3-next-80b-todo.log
-pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project reddit --from interface > archive.qwen3-next-80b-reddit.log
-pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project bbs --from interface > archive.qwen3-next-80b-bbs.log
+pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project todo --from realize > archive.qwen3-next-80b-todo.log
+pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project reddit --from test > archive.qwen3-next-80b-reddit.log
+pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project bbs --from test > archive.qwen3-next-80b-bbs.log
 pnpm run archive --vendor qwen/qwen3-next-80b-a3b-instruct --project shopping --from interface > archive.qwen3-next-80b-shopping.log
