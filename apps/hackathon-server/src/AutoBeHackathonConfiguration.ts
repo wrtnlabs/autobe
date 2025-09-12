be1ab0f2d9582d51e7ea93b06eea32b875f1d060
Jeongho Nam <samchon.github@gmail.com>
2025-09-12 21:18:21 +0900

    feat(hackathon): compiler pool

--- apps/hackathon-server/src/AutoBeHackathonConfiguration.ts
+++ apps/hackathon-server/src/AutoBeHackathonConfiguration.ts
@@ -35,6 +35,7 @@ const environments = new Singleton(() => {
 interface IEnvironments {
   HACKATHON_API_PORT: `${number}`;
   HACKATHON_SEMAPHORE: `${number}`;
+  HACKATHON_COMPILERS: `${number}`;
 
   HACKATHON_JWT_SECRET_KEY: string;
   HACKATHON_JWT_REFRESH_KEY: string;
--- apps/hackathon-server/src/providers/sessions/acceptors/AutoBeHackathonSessionCompiler.ts
+++ apps/hackathon-server/src/providers/sessions/acceptors/AutoBeHackathonSessionCompiler.ts
@@ -1,31 +1,41 @@
 import { IAutoBeCompiler, IAutoBeCompilerListener } from "@autobe/interface";
+import { ArrayUtil, RandomGenerator } from "@nestia/e2e";
 import path from "path";
 import { WorkerConnector } from "tgrid";
 import { Singleton } from "tstl";
 
+import { AutoBeHackathonConfiguration } from "../../../AutoBeHackathonConfiguration";
+
 export namespace AutoBeHackathonSessionCompiler {
-  export const get = (): Promise<IAutoBeCompiler> => compiler.get();
+  export const get = async (): Promise<IAutoBeCompiler> => {
+    return RandomGenerator.pick(await pool.get());
+  };
 }
 
-const compiler = new Singleton(async (): Promise<IAutoBeCompiler> => {
-  const compiler: WorkerConnector<
-    null,
-    IAutoBeCompilerListener,
-    IAutoBeCompiler
-  > = new WorkerConnector(
-    null,
-    {
-      realize: {
-        test: {
-          onOperation: async () => {},
-          onReset: async () => {},
+const pool = new Singleton(() =>
+  ArrayUtil.asyncRepeat(
+    Number(AutoBeHackathonConfiguration.env().HACKATHON_COMPILERS),
+    async () => {
+      const compiler: WorkerConnector<
+        null,
+        IAutoBeCompilerListener,
+        IAutoBeCompiler
+      > = new WorkerConnector(
+        null,
+        {
+          realize: {
+            test: {
+              onOperation: async () => {},
+              onReset: async () => {},
+            },
+          },
         },
-      },
+        "process",
+      );
+      await compiler.connect(
+        `${__dirname}/../../../executable/compiler${path.extname(__filename)}`,
+      );
+      return compiler.getDriver();
     },
-    "process",
-  );
-  await compiler.connect(
-    `${__dirname}/../../../executable/compiler${path.extname(__filename)}`,
-  );
-  return compiler.getDriver();
-});
+  ),
+);
