406910d3e543fd37c3f822de592006f18b6ff54d
Jeongho Nam <samchon.github@gmail.com>
2025-09-15 10:14:17 +0900

    Update model in participant session creation test

--- apps/hackathon-server/test/features/api/participants/test_api_hackathon_participant_session_create_limit.ts
+++ apps/hackathon-server/test/features/api/participants/test_api_hackathon_participant_session_create_limit.ts
@@ -49,7 +49,7 @@ const process = async (connection: HackathonApi.IConnection) => {
         connection,
         TestGlobal.CODE,
         {
-          model: "qwen/qwen3-235b-a22b-2507",
+          model: "qwen/qwen3-next-80b-a3b-instruc",
           timezone: "Asia/Seoul",
         },
       );
