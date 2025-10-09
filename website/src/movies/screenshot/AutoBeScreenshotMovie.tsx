756c2a19422724d3eaf177559682d17d37c9ff71
Jeongho Nam <samchon.github@gmail.com>
2025-10-10 01:17:35 +0900

    complete screenshot

--- website/src/movies/screenshot/AutoBeScreenshotMovie.tsx
+++ website/src/movies/screenshot/AutoBeScreenshotMovie.tsx
@@ -5,12 +5,12 @@ import { useEffect, useState } from "react";
 import AutoBeDemoModelMovie from "../demo/AutoBeDemoModelMovie";
 
 export default function AutoBeScreenshotMovie() {
-  const [model, setModel] = useState("openai/gpt-4.1");
+  const [model, setModel] = useState<string>(window ? getValue() : "");
   useEffect(() => {
-    const value: string | null = new URLSearchParams(
-      window.location.search,
-    ).get("model");
-    if (value) setModel(value);
+    setModel(getValue());
   }, []);
   return <AutoBeDemoModelMovie model={model} />;
 }
+
+const getValue = (): string =>
+  new URLSearchParams(window.location.search).get("model") ?? "openai/gpt-4.1";
