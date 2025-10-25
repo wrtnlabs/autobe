623b564ea70543f5f7d5adb4c437df29df5a377e
Jeongho Nam <samchon.github@gmail.com>
2025-10-26 02:14:22 +0900

    fix typos

--- test/src/features/interface/test_agent_interface_schema_rename_missed.ts
+++ test/src/features/interface/test_agent_interface_schema_rename_missed.ts
@@ -9,10 +9,10 @@ import finalizeJson from "./rename/finalize.json";
 
 export const test_agent_interface_schema_rename_missed = (): void => {
   const before = typia.assert<AutoBeOpenApi.IDocument>(beforeJson);
-  const afer = typia.assert<AutoBeOpenApi.IDocument>(afterJson);
+  const after = typia.assert<AutoBeOpenApi.IDocument>(afterJson);
   const finalize = typia.assert<AutoBeOpenApi.IDocument>(finalizeJson);
 
   TestValidator.equals("before", missedOpenApiSchemas(before), []);
-  TestValidator.equals("after", missedOpenApiSchemas(afer), []);
+  TestValidator.equals("after", missedOpenApiSchemas(after), []);
   TestValidator.equals("finalize", missedOpenApiSchemas(finalize), []);
 };
