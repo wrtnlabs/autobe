6f8a93b0a40adeb348cef0522fc8fb475bbd62d1
michael <7471919@naver.com>
2025-12-02 13:49:06 +0900

    chore: fix transform function about Test Agent

--- packages/agent/src/orchestrate/test/histories/transformTestValidateEvent.ts
+++ packages/agent/src/orchestrate/test/histories/transformTestValidateEvent.ts
@@ -1,25 +1,11 @@
 import { AutoBeTestValidateEvent } from "@autobe/interface";
 
-import { IAutoBeTestFunction } from "../structures/IAutoBeTestFunction";
-import { IAutoBeTestScenarioArtifacts } from "../structures/IAutoBeTestScenarioArtifacts";
+import { IAutoBeTestAgentResult } from "../structures/IAutoBeTestAgentResult";
 
-export const transformTestValidateEvent = (
+export const transformTestValidateEvent = <T extends IAutoBeTestAgentResult>(
   event: AutoBeTestValidateEvent,
-  artifacts: IAutoBeTestScenarioArtifacts,
-): IAutoBeTestFunction => ({
-  scenario:
-    event.function.kind === "write"
-      ? {
-          ...event.function.scenario,
-          functionName: event.function.functionName,
-        }
-      : {
-          dependencies: [],
-          draft: "",
-          endpoint: event.function.endpoint,
-          functionName: event.function.functionName,
-        },
-  artifacts,
-  location: event.function.location,
-  script: event.function.content,
+  item: T,
+): T => ({
+  ...item,
+  function: event.function,
 });
