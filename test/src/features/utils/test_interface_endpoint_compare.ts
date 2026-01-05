88838df2baac55867650634ed655134aa9b21861
Jeongho Nam <samchon.github@gmail.com>
2026-01-05 11:25:59 +0900

    OK

---
+++ test/src/features/utils/test_interface_endpoint_compare.ts
@@ -0,0 +1,19 @@
+import { AutoBeOpenApi } from "@autobe/interface";
+import { AutoBeOpenApiEndpointComparator } from "@autobe/utils";
+import { TestValidator } from "@nestia/e2e";
+
+export const test_interface_endpoint_compare = (): void => {
+  const endpoints: AutoBeOpenApi.IEndpoint[] = [
+    { method: "patch", path: "/customers" },
+    { method: "post", path: "/customers" },
+    { method: "post", path: "/sellers" },
+    { method: "patch", path: "/sellers" },
+  ];
+  endpoints.sort(AutoBeOpenApiEndpointComparator.compare);
+  TestValidator.equals("endpoints", endpoints, [
+    { method: "patch", path: "/customers" },
+    { method: "post", path: "/customers" },
+    { method: "post", path: "/sellers" },
+    { method: "patch", path: "/sellers" },
+  ]);
+};
