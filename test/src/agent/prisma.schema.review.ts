d4c1da41999852b289460d6c158a71117f33c8e1
michael <7471919@naver.com>
2026-01-06 18:04:38 +0900

    test: add prisma.schema.review.ts

---
+++ test/src/agent/prisma.schema.review.ts
@@ -0,0 +1,21 @@
+import { AutoBeExampleProject } from "@autobe/interface";
+import typia from "typia";
+
+import { TestGlobal } from "../TestGlobal";
+import { prepare_prisma_agent } from "./internal/prepare_prisma_agent";
+import { validate_prisma_component_review } from "./internal/validate_prisma_component_review";
+
+const main = async () => {
+  const project: AutoBeExampleProject = typia.assert<AutoBeExampleProject>(
+    TestGlobal.getArguments("project")?.[0] ?? "todo",
+  );
+  await validate_prisma_component_review({
+    agent: await prepare_prisma_agent({
+      vendor: TestGlobal.vendorModel,
+      project,
+    }),
+    vendor: TestGlobal.vendorModel,
+    project,
+  });
+};
+main().catch(console.error);
