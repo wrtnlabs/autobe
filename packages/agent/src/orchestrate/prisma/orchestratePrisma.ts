b77e7e4ee1ae20489d00decd66938ef833b496ef
michael <7471919@naver.com>
2026-01-09 14:25:32 +0900

    chore: remove unnecessary import

--- packages/agent/src/orchestrate/prisma/orchestratePrisma.ts
+++ packages/agent/src/orchestrate/prisma/orchestratePrisma.ts
@@ -3,7 +3,6 @@ import {
   AutoBeDatabase,
   AutoBeDatabaseCompleteEvent,
   AutoBeDatabaseComponent,
-  AutoBeDatabaseComponentReviewEvent,
   AutoBeDatabaseGroup,
   AutoBeDatabaseHistory,
   AutoBeDatabaseSchemaEvent,
