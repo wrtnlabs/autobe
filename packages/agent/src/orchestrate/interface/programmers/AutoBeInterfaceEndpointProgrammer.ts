b3a34b639f50f3b6661b5ddcfdfb8a499e4345f4
Jeongho Nam <samchon.github@gmail.com>
2026-01-22 18:22:45 +0900

    `AutoBeInterfaceEndpointProgrammer.validateDesign()`

--- packages/agent/src/orchestrate/interface/programmers/AutoBeInterfaceEndpointProgrammer.ts
+++ packages/agent/src/orchestrate/interface/programmers/AutoBeInterfaceEndpointProgrammer.ts
@@ -2,7 +2,13 @@ import {
   AutoBeAnalyzeActor,
   AutoBeInterfaceEndpointDesign,
 } from "@autobe/interface";
-import { ILlmApplication, ILlmSchema, LlmTypeChecker } from "@samchon/openapi";
+import { StringUtil } from "@autobe/utils";
+import {
+  ILlmApplication,
+  ILlmSchema,
+  IValidation,
+  LlmTypeChecker,
+} from "@samchon/openapi";
 import typia from "typia";
 
 export namespace AutoBeInterfaceEndpointProgrammer {
@@ -33,5 +39,31 @@ export namespace AutoBeInterfaceEndpointProgrammer {
       type: "string",
       enum: props.actors.map((actor) => actor.name),
     };
+  };
+
+  export const validateDesign = (props: {
+    design: AutoBeInterfaceEndpointDesign;
+    actors: AutoBeAnalyzeActor[];
+    path: string;
+    errors: IValidation.IError[];
+  }): void => {
+    props.design.authorizationActors.forEach((actorName, i) => {
+      if (props.actors.find((actor) => actor.name === actorName) === undefined)
+        props.errors.push({
+          path: `${props.path}.authorizationActors[${i}]`,
+          expected: props.actors
+            .map((a) => a.name)
+            .map((s) => JSON.stringify(s))
+            .join(" | "),
+          value: actorName,
+          description: StringUtil.trim`
+            The actor name must be one of the analyzed actors.
+
+            Avilable actors are as follows:
+
+            ${props.actors.map((actor) => `- ${actor.name}`).join("\n")}
+          `,
+        });
+    });
   };
 }
