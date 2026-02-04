e6a38ba49764dcedf642153e448f657733253f2f
Jeongho Nam <samchon.github@gmail.com>
2026-02-04 14:43:59 +0900

    tracing

--- test/src/agent/internal/validate_test_generate.ts
+++ test/src/agent/internal/validate_test_generate.ts
@@ -60,5 +60,8 @@ export const validate_test_generate = async (props: {
       ["test.generate.json"]: JSON.stringify(generates),
     },
   });
+
+  console.log("Completed", prepares.length, "->", generates.length);
+
   return generates;
 };
